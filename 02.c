e6321cf0b2f94f95cdd1bc0a73064d622c6a384d
rprosa <101232974+rprosa@users.noreply.github.com>
2022-06-08 19:17:14 -0300

    Update 02.c

--- 02.c
+++ 02.c
@@ -1,27 +1,43 @@
 #include <stdio.h>
 
 int main() {
-  float conceito;
-  printf("informe sua nota: ");
-  scanf("%f", &conceito);
-
-  if (conceito >= 9) {
-    printf("conceito A");
-  } else if (conceito >= 8 && conceito < 9) {
-    printf("conceito B");
+  double notas[5];
+  double conceito = 0;
+  for(int i = 0; i < 5; i++){
+    printf("informe sua nota: ");
+    scanf("%lf", &notas[i]);
   }
 
-  else if (conceito >= 7 && conceito < 8) {
+  for(int i = 0; i < 5; i++){
+    conceito += notas[i];
+  };
+
+  conceito = conceito/5;
+
+  if (conceito >= 9.0){
+    printf("conceito A");
+  }
+  else if (conceito >= 8.0 && conceito < 9.0){
+    printf("conceito B");
+  }
+    
+  else if (conceito >= 7.0 && conceito < 8.0) {
     printf("conceito C");
   }
 
-  else if (conceito >= 6 && conceito < 7) {
-    printf("conceito D");
+  else if (conceito >= 6.0 && conceito < 7.0){
+      printf("conceito D");
   }
 
-  else if (conceito < 6) {
-    printf("conceito E");
+  else if (conceito < 6.0){
+      printf("conceito E");
   }
 
+  printf("\nMédia final: %.2lf", conceito);
+
+
+  
+  
   return (0);
 }
+
