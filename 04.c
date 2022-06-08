9a63e5f56e9fe434b10249efc1311510f79a4290
Caio G. Freire <88165211+Cgfreire@users.noreply.github.com>
2022-06-07 21:16:05 -0300

    Update 04.c

--- 04.c
+++ 04.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
-double pi =  3.141592;
+double pi = 3.141592;
 
-double comprimento_circ(int raio){
+double comprimento_circ(int raio) {
 
-    double comprimento_circ_res = (2 * pi)*raio;
+  double comprimento_circ_res = (2 * pi) * raio;
 
-    return comprimento_circ_res;    
+  return comprimento_circ_res;
 }
 
-int main(){
-    double resposta = comprimento_circ(5);
-    printf("Comprinto é: %lf",  resposta);
+int main() {
+
+  int vet[4];
+  for (int j = 0; j < 4; j++) {
+    printf("Insira o valor: ");
+    scanf("%d", &vet[j]);
+  }
+
+  int i;
+
+  for (i = 0; i < sizeof(vet) / sizeof(vet[0]); i++) {
+    double resposta = comprimento_circ(vet[i]);
+    printf("Comprimento é: %lf \n", resposta);
+  }
+
+  return 0;
 }
